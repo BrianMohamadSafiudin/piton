e83b8f5835470006a10f723e385f02c34343a14b
BrianMohamadSafiudin <brianms2004@gmail.com>
2024-05-07 12:24:49 +0700

    update

--- pubsub2.py
+++ pubsub2.py
@@ -9,17 +9,17 @@ port = 1883
 topics = ["esp/mpu6050/acceleration", "esp/mpu6050/gyroscope", "esp/mpu6050/temperature"]
 
 # Initialize or open data file
-if not os.path.exists('sensor_data.json'):
-    with open('sensor_data.json', 'w') as file:
+if not os.path.exists('sensordata.json'):
+    with open('sensordata.json', 'w') as file:
         json.dump([], file)  # Initialize file as JSON array
 
 # Function to load data from JSON file
 def load_data():
-    with open('sensor_data.json', 'r') as json_file:
+    with open('sensordata.json', 'r') as json_file:
         return json.load(json_file)
 
 # Buffer for sensor data and counter
-sensor_data_buffer = {}
+sensordata_buffer = {}
 data_count = 0
 
 # MQTT callback functions
@@ -40,23 +40,23 @@ def on_message(client, userdata, msg):
 
     # Initialize or update the entry
     key = (device_id, timestamp)
-    if key not in sensor_data_buffer:
-        sensor_data_buffer[key] = {"timestamp": timestamp, "device_id": device_id}
+    if key not in sensordata_buffer:
+        sensordata_buffer[key] = {"timestamp": timestamp, "device_id": device_id}
     
     if sensor_type == "acceleration" or sensor_type == "gyroscope":
-        sensor_data_buffer[key][sensor_type] = {"x": data_values[0], "y": data_values[1], "z": data_values[2]}
+        sensordata_buffer[key][sensor_type] = {"x": data_values[0], "y": data_values[1], "z": data_values[2]}
     elif sensor_type == "temperature":
-        sensor_data_buffer[key][sensor_type] = data_values[0]
+        sensordata_buffer[key][sensor_type] = data_values[0]
 
     # Check if entry is complete
-    if all(k in sensor_data_buffer[key] for k in ["acceleration", "gyroscope", "temperature"]):
+    if all(k in sensordata_buffer[key] for k in ["acceleration", "gyroscope", "temperature"]):
         data_count += 1
         if data_count >= 100:  # Adjust this as necessary
             existing_data = load_data()
-            updated_data = existing_data + list(sensor_data_buffer.values())
-            with open('sensor_data.json', 'w') as json_file:
+            updated_data = existing_data + list(sensordata_buffer.values())
+            with open('sensordata.json', 'w') as json_file:
                 json.dump(updated_data, json_file, indent=4)
-            sensor_data_buffer.clear()
+            sensordata_buffer.clear()
             data_count = 0
 
 # Setup MQTT client
