2e28da0adbdb3e1a7b0b6b43ad57100536541196
BrianMohamadSafiudin <brianms2004@gmail.com>
2024-06-04 10:13:16 +0700

    update

--- subscriber.py
+++ subscriber.py
@@ -5,18 +5,18 @@ from firebase_admin import credentials, firestore
 from typing import Final
 from telegram.ext import Application
 
-# Inisialisasi aplikasi Firebase Admin
+# Initialize Firebase Admin application
 cred = credentials.Certificate('falldetectionk4-07f9faa580c1.json')
 firebase_admin.initialize_app(cred)
 db = firestore.client()
 
-# Fungsi untuk membaca data pelanggan dari file JSON
+# Function to read subscriber data from JSON file
 def load_subscribers():
     with open('subscribers.json', 'r') as file:
         data = json.load(file)
     return data
 
-# Fungsi untuk membaca hasil prediksi dari file JSON
+# Function to read prediction data from JSON file
 def load_prediction():
     with open('hasilprediksi.json', 'r') as file:
         data = json.load(file)
@@ -29,19 +29,19 @@ async def send_periodic_messages(app):
     while True:
         subscribers = load_subscribers()
         prediction = load_prediction()
-        print("Mengecek daftar pelanggan yang berlangganan...")
+        print("Checking the list of subscribed customers...")
 
-        if subscribers:  # Hanya kirim pesan jika ada pelanggan yang berlangganan
+        if subscribers:  # Only send messages if there are subscribers
             for subscriber_id in subscribers:
-                print(f"Mengirim pesan periodik ke {subscriber_id}")
-                await app.bot.send_message(subscriber_id, f"Hasil prediksi terbaru: {prediction}")
-                # Mengirim hasil prediksi ke Firebase
+                print(f"Sending periodic message to {subscriber_id}")
+                await app.bot.send_message(subscriber_id, f"Latest prediction result: {prediction}")
+                # Send prediction result to Firebase
                 doc_ref = db.collection('predictions').document(str(subscriber_id))
                 doc_ref.set({
                     'prediction': prediction
                 })
         else:
-            print("Tidak ada pelanggan yang berlangganan.")
+            print("No subscribed customers.")
 
         await asyncio.sleep(6)
 
@@ -49,6 +49,6 @@ if __name__ == '__main__':
     print('Starting bot...')
     app = Application.builder().token(TOKEN).build()
 
-    # Mulai mengirim pesan periodik
+    # Start sending periodic messages
     asyncio.run(send_periodic_messages(app))
-    print('Menjalankan send_periodic_messages()')
+    print('Running send_periodic_messages()')
