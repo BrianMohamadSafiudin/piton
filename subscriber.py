c591f3eac024234aee48b9b82ee44cac316678f3
BrianMohamadSafiudin <brianms2004@gmail.com>
2024-06-04 07:39:24 +0700

    feat: Update

--- subscriber.py
+++ subscriber.py
@@ -1,7 +1,14 @@
 import json
 import asyncio
+import firebase_admin
+from firebase_admin import credentials, firestore
 from typing import Final
 from telegram.ext import Application
+
+# Inisialisasi aplikasi Firebase Admin
+cred = credentials.Certificate('falldetectionk4-07f9faa580c1.json')
+firebase_admin.initialize_app(cred)
+db = firestore.client()
 
 # Fungsi untuk membaca data pelanggan dari file JSON
 def load_subscribers():
@@ -23,11 +30,16 @@ async def send_periodic_messages(app):
         subscribers = load_subscribers()
         prediction = load_prediction()
         print("Mengecek daftar pelanggan yang berlangganan...")
-        
+
         if subscribers:  # Hanya kirim pesan jika ada pelanggan yang berlangganan
             for subscriber_id in subscribers:
                 print(f"Mengirim pesan periodik ke {subscriber_id}")
                 await app.bot.send_message(subscriber_id, f"Hasil prediksi terbaru: {prediction}")
+                # Mengirim hasil prediksi ke Firebase
+                doc_ref = db.collection('predictions').document(subscriber_id)
+                doc_ref.set({
+                    'prediction': prediction
+                })
         else:
             print("Tidak ada pelanggan yang berlangganan.")
 
