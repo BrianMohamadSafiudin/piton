6ddc6e530d2bfe147f0a7eb97f294d9caec2e3e4
BrianMohamadSafiudin <brianms2004@gmail.com>
2024-06-11 09:26:02 +0700

    feat: update

--- subscriber.py
+++ subscriber.py
@@ -1,110 +1,63 @@
 import json
-import os
-import paho.mqtt.client as mqtt
-from datetime import datetime
+import asyncio
 import firebase_admin
-from firebase_admin import credentials, db
+from firebase_admin import credentials, firestore
+from typing import Final
+from telegram.ext import Application
 
-# Configuration for MQTT
-broker_address = "34.101.62.111"
-port = 1883
-topic = "esp/mpu6050/sensors"
+# Initialize Firebase Admin application
+cred = credentials.Certificate('falldetectionk4-07f9faa580c1.json')
+firebase_admin.initialize_app(cred)
+db = firestore.client()
 
-# Firebase configuration
-cred = credentials.Certificate("falldetectionk4-07f9faa580c1.json")  # Ganti dengan path ke file kredensial layanan Anda
-firebase_admin.initialize_app(cred, {
-    'databaseURL': 'https://falldetectionk4-default-rtdb.asia-southeast1.firebasedatabase.app/'
-})
+# Function to read subscriber data from JSON file
+def load_subscribers():
+    with open('subscribers.json', 'r') as file:
+        data = json.load(file)
+    return data
 
-# Initialize or open data file
-if not os.path.exists('sensordata.json'):
-    with open('sensordata.json', 'w') as file:
-        json.dump([], file)
+# Function to read prediction data from JSON file
+def load_prediction():
+    with open('hasilprediksi.json', 'r') as file:
+        data = json.load(file)
+    return data['prediction']
 
-# Function to load data from JSON file
-def load_data():
-    if os.path.exists('sensordata.json'):
-        with open('sensordata.json', 'r') as json_file:
-            # Read the file contents
-            data = json_file.read()
-            # If the file is not empty, load the JSON
-            if data:
-                return json.loads(data)
-            else:
-                return []
-    else:
-        return []
+TOKEN: Final = "6652548059:AAHMjQxSifta_RDNP99XZe3TxOHOFh0gGwA"
+BOT_USERNAME: Final = "@FallDetectionMonitorBot"
 
-# Buffer for sensor data
-sensordata = load_data()
+async def send_periodic_messages(app):
+    while True:
+        subscribers = load_subscribers()
+        prediction = load_prediction()
+        print("Checking the list of subscribed customers...")
 
-# MQTT callback functions
-def on_connect(client, userdata, flags, rc):
-    print("Connected with result code " + str(rc))
-    client.subscribe(topic)
-
-def on_message(client, userdata, msg):
-    print("Message received-> " + msg.topic + " " + str(msg.payload))
-    try:
-        data_str = msg.payload.decode('utf-8')
-        print(f"Decoded data string: {data_str}")  # Debugging line
-        data_parts = data_str.split(',')
-        print(f"Split data parts: {data_parts}")  # Debugging line
-
-        # Validate that the data_parts have exactly 8 elements
-        if len(data_parts) == 8:
-            device_id = data_parts[0]
-            timestamp = datetime.now().isoformat()
-            try:
-                accel_values = [float(i) for i in data_parts[1:4]]
-                gyro_values = [float(i) for i in data_parts[4:7]]
-                temp_value = float(data_parts[7])
-            except ValueError as e:
-                print(f"Error converting data parts to float: {e}")
-                print(f"Data parts: {data_parts}")
-                return
-
-            sensor_entry = {
-                "timestamp": timestamp,
-                "device_id": device_id,
-                "acceleration.x": accel_values[0],
-                "acceleration.y": accel_values[1],
-                "acceleration.z": accel_values[2],
-                "gyroscope.x": gyro_values[0],
-                "gyroscope.y": gyro_values[1],
-                "gyroscope.z": gyro_values[2],
-                "temperature": temp_value
-            }
-
-            print(f"Formatted sensor entry: {json.dumps(sensor_entry, indent=4)}")  # Debugging line
-
-            # Append the new sensor entry
-            sensordata.append(sensor_entry)
-
-            # Maintain only the last 30 entries
-            if len(sensordata) > 25:
-                sensordata.pop(0)
-
-            # Overwrite the JSON file with the updated sensor data
-            with open('sensordata.json', 'w') as json_file:
-                json.dump(sensordata, json_file, indent=4)
-
-            # Push data to Firebase Realtime Database
-            ref = db.reference(f'/sensors/{device_id}')
-            result = ref.push(sensor_entry)
-            print(f"Data pushed to Firebase with result: {result}")  # Debugging line
+        # Check if the prediction is "Stand for 30 seconds (D01/01)" or "Walk normally and turn for 4m (D06/06)"
+        if prediction in ["Stand for 30 seconds (D01/01)", "Stand normally and walk (D06/06)"]:
+            for subscriber_id in subscribers:
+                # Send prediction result to Firebase
+                doc_ref = db.collection('predictions').document(str(subscriber_id))
+                doc_ref.set({
+                    'prediction': prediction
+                })           
+                print(f"Prediction result is '{prediction}'. Skipping message sending.")
+        elif subscribers:  # Only send messages if there are subscribers
+            for subscriber_id in subscribers:
+                print(f"Sending periodic message to {subscriber_id}")
+                await app.bot.send_message(subscriber_id, f"Darurat!! Hasil prediksi: {prediction}")
+                # Send prediction result to Firebase
+                doc_ref = db.collection('predictions').document(str(subscriber_id))
+                doc_ref.set({
+                    'prediction': prediction
+                })
         else:
-            print("Invalid data format received: ", data_parts)
-    except ValueError as e:
-        print("Error parsing data: ", e)
-        print("Received data string: ", data_str)
-    except Exception as e:
-        print("Error processing message: ", e)
-        print("Received data string: ", data_str)
+            print("No subscribed customers.")
 
-# Setup MQTT client
-client = mqtt.Client()
-client.on_connect = on_connect
-client.on_message = on_message
-client.connect(broker_address, port=port)
-client.loop_forever()
+        await asyncio.sleep(3)
+
+if __name__ == '__main__':
+    print('Starting bot...')
+    app = Application.builder().token(TOKEN).build()
+
+    # Start sending periodic messages
+    asyncio.run(send_periodic_messages(app))
+    print('Running send_periodic_messages()')
